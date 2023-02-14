633e5ff0fd713042ca6a7c382b442d1ce835c7aa
Victor Novakoski <Novakoski2021@gmail.com>
2023-02-14 11:59:34 -0300

    fixed:add all functionalities

--- pets/serializers.py
+++ pets/serializers.py
@@ -7,11 +7,10 @@ from traits.serializers import TraitSerializer
 class PetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=50)
-    age = serializers.IntegerField(max_length=10)
-    weight = serializers.FloatField(max_length=30)
-    sex = serializers.CharField(
-        max_length=20,
+    age = serializers.IntegerField()
+    weight = serializers.FloatField()
+    sex = serializers.ChoiceField(
         choices=Sex.choices,
         default=Sex.DEFAULT)
-    # group = GroupSerializer(many=True, read_only=True)
-    # trait = TraitSerializer(read_only=True)
+    group = GroupSerializer()
+    traits = TraitSerializer(many=True)
--- traits/models.py
+++ traits/models.py
@@ -7,7 +7,7 @@ class Trait(models.Model):
     name = models.CharField(max_length=20, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    pet = models.ManyToManyField(
+    pets = models.ManyToManyField(
         "pets.Pet",
         related_name="traits",
     )
